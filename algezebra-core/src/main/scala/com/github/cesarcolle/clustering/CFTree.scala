7abd1cd8d2a47775cfcbf7d7893f7fc77e02ca6a
cesar colle <cesar.colle@gmail.com>
2018-09-26 13:26:03 +0200

    Add first distance

--- algezebra-core/src/main/scala/com/github/cesarcolle/clustering/CFTree.scala
+++ algezebra-core/src/main/scala/com/github/cesarcolle/clustering/CFTree.scala
@@ -1,14 +1,87 @@
 package com.github.cesarcolle.clustering
 
-case class CFNode(maxEntries : Int, distThreshold : Double,
-                  distFunction : Int,
-                  merging : Boolean,
-                  leafStatus : Boolean,
+sealed trait DistFunction {
+  def distance(e1: CFEntry, e2: CFEntry): Double
+
+
+  def add4Vectors(v1 : Vector[Double], v2 : Vector[Double],  v3: Vector[Double], v4 : Vector[Double]) : Vector[(Double, Double)] = {
+    (v1 zip v2 zip v3 zip v4)
+      .map { case (((a, b), c), d) => (a, b, c, d) }
+      .map(tuples => (tuples._1 + tuples._2, tuples._3 + tuples._4 ))
+  }
+}
+
+case class DistZero() extends DistFunction {
+  override def distance(e1: CFEntry, e2: CFEntry): Double = {
+    require(e1.sumX.size == e2.sumX.size, "can't compute dist on different Entries ...")
+
+    val dist = e1.sumX.zip(e2.sumX).map(sums => Math.pow(sums._1 / e1.n - sums._2 / e2.n, 2)).sum
+    require(dist > 0, "dist0 can't be negative..")
+    Math.sqrt(dist)
+  }
+}
+
+case class DistOne() extends DistFunction {
+  override def distance(e1: CFEntry, e2: CFEntry): Double = {
+    require(e1.sumX.size == e2.sumX.size, "can't compute dist on different Entries ...")
+    val dist = e1.sumX.zip(e2.sumX).map(sums => (sums._1 / e1.n - sums._2 / e2.n).abs).sum
+    require(dist > 0, "distOne can't be negative")
+
+    dist
+  }
+}
+
+case class DistTwo() extends DistFunction {
+  override def distance(e1: CFEntry, e2: CFEntry): Double = {
+    require(e1.sumX.size == e2.sumX.size, "can't compute dist on different Entries ...")
+    val n1 = e1.n
+    val n2 = e1.n
+    val flat = (e1.sumX zip e2.sumX zip e1.sumX2 zip e2.sumX2) map { case (((a, b), c), d) => (a, b, c, d) }
+
+    val dist = flat.map(tuples => (n2 * tuples._3 - 2 * tuples._1 * tuples._2 + n1 * tuples._4) / (n1 * n2)).sum
+    require(dist > 0, "distOne can't be negative")
+
+    dist
+  }
+}
+
+case class DistThree() extends DistFunction {
+  override def distance(e1: CFEntry, e2: CFEntry): Double = {
+    val n1 = e1.n
+    val n2 = e1.n
+
+    val flatX1 =add4Vectors(e1.sumX, e2.sumX, e1.sumX2, e2.sumX2)
+    val dist = flatX1.map(totX => ((n1 + n2) * totX._2 - 2 * Math.pow(totX._1, 2) + (n1 + n2) * totX._2)
+      / ((n1 + n2)* (n1 + n2 -1) )).sum
+    require(dist > 0, "distOne can't be negative")
+
+    dist
+  }
+}
+
+case class DistFour() extends DistFunction {
+  override def distance(e1: CFEntry, e2: CFEntry): Double = {
+      val n1 = e1.n
+      val n2 = e2.n
+     val tots = add4Vectors(e1.sumX, e2.sumX, e1.sumX2, e2.sumX2)
+//    double diff1 = totSumX2[i] - 2*totSumX[i]*totSumX[i]/(n1+n2) + (n1+n2)*(totSumX[i]/(n1+n2))*(totSumX[i]/(n1+n2));
+//    double diff2 = e1.sumX2[i] - 2*e1.sumX[i]*e1.sumX[i]/n1 + n1*(e1.sumX[i]/n1)*(e1.sumX[i]/n1);
+//    double diff3 = e2.sumX2[i] - 2*e2.sumX[i]*e2.sumX[i]/n2 + n2*(e2.sumX[i]/n2)*(e2.sumX[i]/n2);
+//    dist += diff1 - diff2 - diff3;
+    tots.map { tot =>
+      val diff1 = tot._2 - 2 * tot._1 * tot._1 / (n1 + n2) + (n1 + n2) * (tot._1 / n1 + n2) * (tot._1 / (n1 + n2))
+      1
+    }
+
+  }
+}
+
+
+case class CFNode(maxEntries: Int, distThreshold: Double,
+                  distFunction: DistFunction,
+                  merging: Boolean,
+                  leafStatus: Boolean,
                   entries: Vector[CFEntry] = Vector.empty[CFEntry]) {
-
-
-
-
 }
 
 object CFEntry {
